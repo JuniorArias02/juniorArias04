04b5e4415a1bec1cb32fc21553be0f71a7ab0307
juniorArias02 <junior.arias02yt@gmail.com>
2025-07-20 15:08:40 -0500

    fix: correcion de nombre de archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import RUTAS from "./router/router";
-import Layout from "./layouts/Layout";
 import Home from "./page/home/Home";
 import Proyectos from "./page/proyects/Proyectos";
 import Error404 from "./PageError/404";
+import MainLayout from "./layouts/layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path={RUTAS.INICIO} element={<Layout />}>
+      <Route path={RUTAS.INICIO} element={<MainLayout />}>
         <Route index element={<Home />} />
         <Route path={RUTAS.PROYECTOS} element={<Proyectos />} />
         <Route path="*" element={<Error404 />} />
